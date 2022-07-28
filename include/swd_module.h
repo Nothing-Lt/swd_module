ab3696137ea9ca1bfcd82964f59c204b691e7ddb
Li Naqin <Nothing-Lt@users.noreply.github.com>
2022-07-28 12:09:23 +0900

    Update swd_module.h
    Add unhalt_core to ioctl call.

--- include/swd_module.h
+++ include/swd_module.h
@@ -12,12 +12,13 @@ struct swd_parameters
 #define SWDDEV_IOC_RSTLN    _IO(SWDDEV_IOC_MAGIC, 0)            //  1. reset line
 #define SWDDEV_IOC_RDDPREG  _IOR(SWDDEV_IOC_MAGIC, 1, struct swd_parameters)      //  2. read dp reg
 #define SWDDEV_IOC_WRDPREG  _IOW(SWDDEV_IOC_MAGIC, 2, struct swd_parameters)      //  3. write dp reg
-#define SWDDEV_IOC_HLTCORE  _IO(SWDDEV_IOC_MAGIC, 3)            //  4. halt core
-#define SWDDEV_IOC_TSTALIVE _IOR(SWDDEV_IOC_MAGIC, 4, struct swd_parameters)      //  5. test alive
-#define SWDDEV_IOC_SETBASE  _IOW(SWDDEV_IOC_MAGIC, 5, struct swd_parameters)       //  6. set base
-#define SWDDEV_IOC_DWNLDSRAM    _IOW(SWDDEV_IOC_MAGIC, 6, struct swd_parameters)  //  7. download to sram
-#define SWDDEV_IOC_DWNLDFLSH    _IOW(SWDDEV_IOC_MAGIC, 7, struct swd_parameters)  //  8. download to flash
-#define SWDDEV_IOC_ERSFLSH      _IO(SWDDEV_IOC_MAGIC, 8)        //  9. erase flash
-#define SWDDEV_IOC_VRFY         _IOR(SWDDEV_IOC_MAGIC, 9, struct swd_parameters)  //  10. verify
+#define SWDDEV_IOC_HLTCORE  _IO(SWDDEV_IOC_MAGIC, 3)            //  4. halt core  //  3. write dp reg
+#define SWDDEV_IOC_UNHLTCORE  _IO(SWDDEV_IOC_MAGIC, 4)            //  4. halt core
+#define SWDDEV_IOC_TSTALIVE _IOR(SWDDEV_IOC_MAGIC, 5, struct swd_parameters)      //  5. test alive
+#define SWDDEV_IOC_SETBASE  _IOW(SWDDEV_IOC_MAGIC, 6, struct swd_parameters)       //  6. set base
+#define SWDDEV_IOC_DWNLDSRAM    _IOW(SWDDEV_IOC_MAGIC, 7, struct swd_parameters)  //  7. download to sram
+#define SWDDEV_IOC_DWNLDFLSH    _IOW(SWDDEV_IOC_MAGIC, 8, struct swd_parameters)  //  8. download to flash
+#define SWDDEV_IOC_ERSFLSH      _IO(SWDDEV_IOC_MAGIC, 9)        //  9. erase flash
+#define SWDDEV_IOC_VRFY         _IOR(SWDDEV_IOC_MAGIC, 10, struct swd_parameters)  //  10. verify
 
 #endif
