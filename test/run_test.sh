c5c4eeced44f5151d25f7c873e2fa79463eeffd2
Li Naqin <Nothing-Lt@users.noreply.github.com>
2022-07-28 12:08:36 +0900

    Makefile & run_test.sh
    Add new test case in Makefile and run_test.sh.

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,22 +1,34 @@
 #!/bin/sh
 
-echo "Test Alive"
+echo "============== Test Alive =============="
 ./main_test_alive
+echo ""
 
-echo "main(read default)"
+echo "============== main(read default) =============="
 ./main
+echo ""
 
-echo "Set Base"
+echo "============== Set Base =============="
 ./main_setbase
+echo ""
 
-echo "Read DP"
+echo "============== Read DP =============="
 ./main_read_dp
+echo ""
 
-echo "Write DP"
+echo "============== Write DP =============="
 ./main_write_dp
+echo ""
 
-echo "RAM Download"
+echo "============== RAM Download =============="
 ./main_ram_download
+echo ""
 
-echo "Flash Download"
+echo "============== Flash Download =============="
 ./main_write_flash
+echo ""
+
+echo "============== Download program to flash =============="
+./main_program_flash
+echo ""
+
